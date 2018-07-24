8be40a776f9f91e2c8f148354bca607587d3ba4c
yxd1 <yxd1@meitu.com>
2018-07-24 20:21:44 +0800

    fix packageName

--- src/main/java/cn/simyy/jfisher/sample/App.java
+++ src/main/java/cn/simyy/jfisher/sample/App.java
@@ -6,7 +6,7 @@ import cn.simyy.jfisher.JFisher;
 public class App implements Bootstrap {
     @Override
     public void init(JFisher jFisher) {
-        jFisher.initDriven("com.simyy.jfisher.sample");
+        jFisher.initDriven("cn.simyy.jfisher.sample");
         //HelloController helloController = new HelloController();
         //jfisher.addRoute("/", "sayHello", helloController);
     }
